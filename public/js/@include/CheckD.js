5c544de720277bdae3093070c8752a6d52cb1a65
kamilatte <118631823+kamilatte@users.noreply.github.com>
2023-10-29 16:53:55 +0200

    Create CheckD.js

---
+++ public/js/@include/CheckD.js
@@ -0,0 +1,18 @@
+function replaceCheckboxesWithImages() {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+        checkboxes.forEach(checkbox => {
+            if (checkbox.hasAttribute('checked') && checkbox.hasAttribute('disabled')) {
+              checkbox.outerHTML = '<img src="/design/yes.jpg" width="25" height="25">';
+            } else if (checkbox.hasAttribute('disabled')) {
+                checkbox.outerHTML = '<img src="/design/no.jpg" width="25" height="25">';
+            }
+        });
+
+        // Remove the script element
+        const scriptElement = document.currentScript;
+        if (scriptElement) {
+            scriptElement.parentElement.removeChild(scriptElement);
+        }
+    }
+    replaceCheckboxesWithImages();
