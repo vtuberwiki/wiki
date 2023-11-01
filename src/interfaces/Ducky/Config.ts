05d1407c21888cc1ac794159c49a1198b057daad
ステラ <live.hylia@outlook.com>
2023-11-01 17:48:24 -0400

    OpenSourceData

--- src/interfaces/Ducky/Config.ts
+++ src/interfaces/Ducky/Config.ts
@@ -1,4 +1,36 @@
-export default interface AppConfig {
+import { bytesToSize } from 'developer-toolkit-utils';
+
+interface DuckyFileExtensions {
+    name: string;
+    description: string;
+    extension: string;
+}
+
+export interface OpenSourceData {
+    id: number,
+    node_id: string,
+    name: string,
+    slug: string,
+    description: string,
+    html_url: string,
+    clone_url: string,
+    size: string,
+    homepage: string,
+    stars_count: string,
+    open_issues_count: string,
+    forks_count: string,
+    language: string,
+    license: {
+        name: string,
+        spdx_id: string,
+        url: string,
+        node_id: string
+    },
+    topics: string[]
+}
+
+export interface AppConfig {
     Version: string;
-    UpdateUrl: string;
+    FileExtensions: DuckyFileExtensions[];
+    OpenSourceData: OpenSourceData;
 }
