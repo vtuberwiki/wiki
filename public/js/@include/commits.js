a4c857c491f34f4089fc5dfc2730b0ebae3fc55f
ステラ <withervt@outlook.com>
2023-12-06 01:06:24 -0500

    Add snackbar element and update SEO in Layout.astro

---
+++ public/js/@include/commits.js
@@ -0,0 +1,48 @@
+(async () => {
+    const snackbar = document.getElementById("snackbar");
+
+    function AddGithubData(commit) {
+      const currentTime = new Date().getTime();
+      localStorage.setItem("X-GitHub-Commit", commit);
+      localStorage.setItem("X-GitHub-Commit-Time", currentTime.toString());
+    }
+
+    function CheckIfShouldShow() {
+      // Check if X-GitHub-Commit exists and if it less than 5 minutes
+      const commit = localStorage.getItem("X-GitHub-Commit");
+
+      if (commit) {
+        const commitTime = localStorage.getItem("X-GitHub-Commit-Time");
+        const currentTime = new Date().getTime();
+        const diff = currentTime - parseInt(commitTime);
+
+        if (diff < 300000) {
+          return false;
+        } else {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    }
+
+    const shouldShow = CheckIfShouldShow();
+
+    if (shouldShow) {
+      fetch("https://api.github.com/repos/vtuberwiki/wiki/commits")
+        .then((res) => res.json())
+        .then((data) => {
+          const commit = data[0];
+          const sha = commit.sha;
+          const message = commit.commit.message;
+          snackbar.innerHTML = `Latest Commit: <a href="https://github.com/vtuberwiki/wiki/commit/${sha}" style="color: white;">${message}</a>`;
+          snackbar.classList.add("show");
+
+          AddGithubData(sha);
+
+          setTimeout(() => {
+            snackbar.classList.remove("show");
+          }, 2700);
+        });
+    }
+  })();
